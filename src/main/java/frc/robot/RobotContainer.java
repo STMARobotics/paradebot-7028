b2a37fdf2358d9c4ab613ba43b3d25406581b660
Andrew Gasser <agasser@users.noreply.github.com>
2021-02-25 20:17:54 -0600

    Turret teleop drive and untested hold position (#6)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,8 +12,10 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ShootCommand;
 import frc.robot.commands.TeleOpDriveCommand;
+import frc.robot.commands.TeleOpTurretCommand;
 import frc.robot.subsystems.CannonSubsystem;
 import frc.robot.subsystems.DriveTrainSubsystem;
+import frc.robot.subsystems.TurretSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -28,7 +30,9 @@ public class RobotContainer {
   private final XboxController driverController = new XboxController(DEVICE_ID_DRIVER_CONTROLLER);
   private final DriveTrainSubsystem driveTrainSubsystem = new DriveTrainSubsystem();
   private final CannonSubsystem cannonSubsystem = new CannonSubsystem();
+  private final TurretSubsystem turretSubsystem = new TurretSubsystem();
   private final TeleOpDriveCommand teleOpDriveCommand = new TeleOpDriveCommand(driveTrainSubsystem, driverController);
+  private final TeleOpTurretCommand teleOpTurretCommand = new TeleOpTurretCommand(turretSubsystem, driverController);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -37,6 +41,7 @@ public class RobotContainer {
     // Configure the button bindings
     configureButtonBindings();
     driveTrainSubsystem.setDefaultCommand(teleOpDriveCommand);
+    turretSubsystem.setDefaultCommand(teleOpTurretCommand);
   }
 
   /**
---
+++ src/main/java/frc/robot/commands/TeleOpTurretCommand.java
@@ -0,0 +1,41 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.TurretSubsystem;
+
+public class TeleOpTurretCommand extends CommandBase {
+
+  private final TurretSubsystem turretSubsystem;
+
+  private final XboxController xboxController;
+
+  public TeleOpTurretCommand(TurretSubsystem turretSubsystem, XboxController xboxController) {
+    this.turretSubsystem = turretSubsystem;
+    this.xboxController = xboxController;
+    addRequirements(turretSubsystem);
+  }
+
+  @Override
+  public void execute() {
+    double leftTrigger = xboxController.getTriggerAxis(Hand.kLeft);
+    double rightTrigger =  xboxController.getTriggerAxis(Hand.kRight);
+    if (leftTrigger > rightTrigger) {
+      turretSubsystem.rotate(-leftTrigger / 12);
+    } else {
+      turretSubsystem.rotate(rightTrigger / 12);
+    }
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    turretSubsystem.rotate(0);
+  }
+  
+}
