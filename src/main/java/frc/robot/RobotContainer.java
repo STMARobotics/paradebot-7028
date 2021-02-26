c408f2c09f4f8714158e3e3cdb8b3b7c5eeadd81
Andrew Gasser <agasser01@hotmail.com>
2021-02-25 20:22:56 -0600

    Raise/lower shooter

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -53,6 +53,11 @@ public class RobotContainer {
   private void configureButtonBindings() {
     new JoystickButton(driverController, XboxController.Button.kA.value)
         .whenPressed(new ShootCommand(cannonSubsystem).withTimeout(VALVE_OPEN_TIME));
+    
+    new JoystickButton(driverController, XboxController.Button.kBumperLeft.value)
+        .whenPressed(() -> cannonSubsystem.raiseCannonToMax(), cannonSubsystem);
+    new JoystickButton(driverController, XboxController.Button.kBumperRight.value)
+        .whenPressed(() -> cannonSubsystem.lowerCannonToMin(), cannonSubsystem);
   }
 
 }
