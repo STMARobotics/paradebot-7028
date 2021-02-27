064a7db6748288a14f200a6cd321538b8945a907
STMA Robotics <7028Robotics 7028Robotics@mystma.org>
2021-02-27 14:34:50 -0600

    Working Sound

--- src/main/java/frc/robot/commands/TeleOpDriveCommand.java
+++ src/main/java/frc/robot/commands/TeleOpDriveCommand.java
@@ -9,7 +9,7 @@ public class TeleOpDriveCommand extends CommandBase {
     
     private final DriveTrainSubsystem driveTrainSubsystem;
     private final XboxController xboxController;
-    
+    private PlaySound activePlaySound;
     public TeleOpDriveCommand(DriveTrainSubsystem driveTrainSubsystem, XboxController xboxController) {
         addRequirements(driveTrainSubsystem);
         this.driveTrainSubsystem = driveTrainSubsystem;
@@ -20,10 +20,27 @@ public class TeleOpDriveCommand extends CommandBase {
     public void initialize() {
        
     }
-
+    public void playAudio(String clip){
+      if (activePlaySound == null || !activePlaySound.getClip().equals(clip)){
+        if (activePlaySound != null){
+          activePlaySound.cancel();
+        }
+        activePlaySound = new PlaySound(clip);
+        activePlaySound.schedule();
+      }
+    }
     @Override
     public void execute() {
         driveTrainSubsystem.drive(-xboxController.getY(Hand.kLeft), xboxController.getX(Hand.kRight));
+        if (Math.abs(xboxController.getY(Hand.kLeft)) > 0.7){
+          playAudio("fast");
+        }
+        else if (Math.abs(xboxController.getY(Hand.kLeft)) > 0.25){
+          playAudio("slow");
+        }
+        else {
+          playAudio("idle");
+        }
     }
 
     @Override
