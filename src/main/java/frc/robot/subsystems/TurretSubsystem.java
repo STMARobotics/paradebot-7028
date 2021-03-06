fee7e822408d9adfc01add8f9c8afb1855cbf0e9
Andrew Gasser <agasser@users.noreply.github.com>
2021-03-06 15:02:39 -0600

    Turret pigeon (#10)
    * Use Pigeon with encoder plugged into Talon
    * Turret raise/lower with two actuators
    
    Co-authored-by: Brandon Gasser <bgasser12@gmail.com>

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -1,12 +1,16 @@
 package frc.robot.subsystems;
 
-import static frc.robot.Constants.TurretConstants.DEVICE_ID_CANNON_ACTUATOR;
+import static frc.robot.Constants.TurretConstants.DEVICE_ID_CANNON_ACTUATOR_ONE;
+import static frc.robot.Constants.TurretConstants.DEVICE_ID_CANNON_ACTUATOR_TWO;
 import static frc.robot.Constants.TurretConstants.DEVICE_ID_PIGEON;
 import static frc.robot.Constants.TurretConstants.DEVICE_ID_TURRET;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
 import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
@@ -18,17 +22,35 @@ public class TurretSubsystem extends SubsystemBase {
 
   private final WPI_TalonSRX turret = new WPI_TalonSRX(DEVICE_ID_TURRET);
   private final PigeonIMU pigeon = new PigeonIMU(DEVICE_ID_PIGEON);
-  private final Servo actuator = new Servo(DEVICE_ID_CANNON_ACTUATOR);
+  private final Servo actuatorOne = new Servo(DEVICE_ID_CANNON_ACTUATOR_ONE);
+  private final Servo actuatorTwo = new Servo(DEVICE_ID_CANNON_ACTUATOR_TWO);
 
   public TurretSubsystem() {
+    TalonSRXConfiguration turretTalonConfig = new TalonSRXConfiguration();
+    // Set encoder as primary so soft limits can be used
+    turretTalonConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.QuadEncoder;
+    // Primary PID all zeros to cancel out PID with encoder
+    turretTalonConfig.slot0.kP = 0d;
+    turretTalonConfig.slot0.kI = 0d;
+    turretTalonConfig.slot0.kD = 0d;
+
+    // Aux PID for Pigeon
+    turretTalonConfig.auxiliaryPID.selectedFeedbackSensor = FeedbackDevice.RemoteSensor0;
+    turretTalonConfig.remoteFilter0.remoteSensorDeviceID = pigeon.getDeviceID();
+    turretTalonConfig.remoteFilter0.remoteSensorSource = RemoteSensorSource.GadgeteerPigeon_Yaw;
+    turretTalonConfig.slot1.kP = .01;
+    turretTalonConfig.slot1.kI = 0d;
+    turretTalonConfig.slot1.kD = 0d;
+
+    turretTalonConfig.reverseLimitSwitchNormal = LimitSwitchNormal.NormallyClosed;
+    turretTalonConfig.forwardLimitSwitchNormal = LimitSwitchNormal.NormallyClosed;
+
     turret.configFactoryDefault();
-    turret.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+    turret.configAllSettings(turretTalonConfig);
     turret.setSensorPhase(true);
 
-    turret.configSelectedFeedbackSensor(FeedbackDevice.RemoteSensor0, 1, 0);
-    turret.configRemoteFeedbackFilter(pigeon.getDeviceID(), RemoteSensorSource.GadgeteerPigeon_Yaw, 0);
-
-    actuator.setBounds(2.0, 1.8, 1.525, 1.25, 1.05);
+    actuatorOne.setBounds(2.0, 1.8, 1.525, 1.25, 1.05);
+    actuatorTwo.setBounds(2.0, 1.8, 1.525, 1.25, 1.05);
   }
 
   @Override
@@ -38,14 +60,21 @@ public class TurretSubsystem extends SubsystemBase {
     SmartDashboard.putNumber("Yaw", ypr[0]);
   }
 
-  public double getPosition() {
-    double[] position = new double[3];
-    pigeon.getYawPitchRoll(position);
-    return position[0];
+  /**
+   * Gets the gyro position in sensor native units (8192 units per rotation)
+   * @return position
+   */
+  public double getGyroPosition() {
+    return turret.getSelectedSensorPosition(1);
   }
 
-  public void setPosition(double position) {
-    turret.set(ControlMode.Position, position);    
+  /**
+   * Sets the position based in the gyro native units
+   * @param position position to set in gyro native units
+   */
+  public void setPositionWithGyro(double position) {
+    // Only aux PID is used since that's where the Pigeon is selected
+    turret.set(ControlMode.Position, 0, DemandType.AuxPID, position);
   }
 
   public void rotate(double speed) {
@@ -57,11 +86,13 @@ public class TurretSubsystem extends SubsystemBase {
   }
 
   public void raiseCannonToMax() {
-    actuator.setSpeed(1.0);
+    actuatorOne.setSpeed(1.0);
+    actuatorTwo.setSpeed(1.0);
   }
 
   public void lowerCannonToMin() {
-    actuator.setSpeed(-1.0);
+    actuatorOne.setSpeed(-1.0);
+    actuatorTwo.setSpeed(-1.0);
   }
 
 }
